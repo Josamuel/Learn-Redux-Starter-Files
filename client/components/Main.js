6115b71ac65c40822956c6f21b6f1661f9665f46
Joey Steinberger <josamuel@umich.edu>
2016-11-16 14:25:34 -0600

    cleaning up files, added to ReadMe

---
+++ client/components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router'
+
+class Main extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+        <Link to="/"> SUXstagram lol</Link>
+        </h1>
+      {/* props will be undefined until we pass in children*/}
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
+    )
+  }
+}
+
+export default Main;
