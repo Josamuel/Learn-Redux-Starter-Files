cb5fa3e5a8e80adcc6fb260295f394c2b69c56c3
Joey Steinberger <josamuel@umich.edu>
2016-11-16 14:22:34 -0600

    added hot module reload for reducers and access to redux dev tools in chrome

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Photo from './Photo'
+import Comments from './Comments'
 
 const Single = React.createClass({
   render() {
+    // i is the index of the component in the photoGrid array.
+    const {postId} = this.props.params;
+    const i = this.props.posts.findIndex((post) =>
+        post.code === postId)
+    console.log('i is', i);
+    // should be the post we click on.
+    const post = this.props.posts[i];
+    console.log('post is', post);
+    // the postID is the specific photo a user clicks on, so this
+    // should find all the comments for this specific post.  
+    // if there are no comments for this photo, postComments will
+    // be an empty array.
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-        I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props}/>
       </div>
     )
   }
